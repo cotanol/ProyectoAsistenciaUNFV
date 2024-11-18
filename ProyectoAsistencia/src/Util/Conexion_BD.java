c95d97c7480e2aa95303442f52dd3f31e63ec82a
cotanol <salcedomoralesfranciscojose@gmail.com>
2024-11-18 18:20:12 -0500

    Sin importancia

--- ProyectoAsistencia/src/Util/Conexion_BD.java
+++ ProyectoAsistencia/src/Util/Conexion_BD.java
@@ -14,7 +14,7 @@ public class Conexion_BD {
             Class.forName("com.mysql.cj.jdbc.Driver"); 
             String url = "jdbc:mysql://localhost:3306/unfv_bd";
             String user = "root";
-            String password = "rafaelgatogordo"; 
+            String password = ""; 
 
             // Intentar conectar
             cn = DriverManager.getConnection(url, user, password);
